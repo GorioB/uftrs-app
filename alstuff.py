c5416ed4552f1915d57321336eaf51fa957f5a3b
Al Lasco <avlasco@up.edu.ph>
2014-06-12 22:45:33 +0800

    working User class

--- alstuff.py
+++ alstuff.py
@@ -1,30 +1,76 @@
-# to be merged into the main DB script later on
 import sqlite3
 import hashlib
 
-dbName = "dbName"
-conn = sqlite3.connect(dbName)
+DB_NAME = "dbName"
 
-class DataField(object):
-	"""docstring for DataField"""
-	def __init__(self, username, password, isRoot=0):
+class User(object):
+	"""docstring for User. Database table name is users"""
+	def __init__(self, username, password):
 		self.username = username
 		self.password = password
-		self.isRoot = isRoot
-		self.cursor = conn.cursor()
-		
-	def saveUser(self):
-		"""Saves the user into the database"""
-		# TODO: if (table doesn't exist) create table (username text, passwordHash text, isRoot integer)
-		self.cursor.execute("INSERT INTO users VALUES (?, ?, ?)", (self.username, self.passwordHash, self.isRoot))
+
+
+	def saveUser(self, isRoot=0):
+		"""Saves/updates the user into the database. Optional: pass a 0 or 1 to specify isRoot value (0 by default)"""
+		conn = sqlite3.connect(DB_NAME)
+		cursor = conn.cursor()
+		if isRoot!=0: isRoot=1
+
+		cursor.execute("CREATE TABLE IF NOT EXISTS users(username TEXT PRIMARY KEY, passwordHash TEXT, isRoot INTEGER)")
+		if self.userExists(self.username):
+			cursor.execute("UPDATE users SET username=?, passwordHash=?, isRoot=? WHERE username=?", 
+				(self.username, self.passwordHash, isRoot, self.username))
+		else:
+			cursor.execute("INSERT INTO users VALUES (?, ?, ?)", (self.username, self.passwordHash, isRoot))
+
+		conn.commit()
+		conn.close()
+
 
 	def auth(self):
-		"""Checks if the username-password combination is valid"""
+		"""Returns True if the User instance's username-password combination is valid"""
+		conn = sqlite3.connect(DB_NAME)
+		cursor = conn.cursor()
+		cursor.execute("SELECT passwordHash FROM users WHERE username=?", (self.username,))
+		passwordHash = cursor.fetchone()
+		conn.close()
+
+		if passwordHash==None: return False
+		return passwordHash[0]==self.passwordHash
+
 
 	def checkIfRoot(self):
-		"""Checks against the database if user is root"""
-		if ~self.auth(): return False
-		# TODO: check against database's isRoot
+		"""Returns True if User instance is root and username-pass combo is valid"""
+		if not self.auth(): return False
+		conn = sqlite3.connect(DB_NAME)
+		cursor = conn.cursor()
+		cursor.execute("SELECT isRoot FROM users WHERE username=?", (self.username,))
+		isRoot = cursor.fetchone()
+		conn.close()
+		return isRoot[0]==1
+
+
+	@staticmethod
+	def listUsers():
+		"""Returns the list of users in the database"""
+		conn = sqlite3.connect(DB_NAME)
+		cursor = conn.cursor()
+		cursor.execute("SELECT * FROM users")
+		userList = cursor.fetchall()
+		conn.close()
+		return userList
+
+
+	@staticmethod
+	def userExists(username):
+		"""Returns True if the passed username exists in the database"""
+		conn = sqlite3.connect(DB_NAME)
+		cursor = conn.cursor()
+		cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+		user = cursor.fetchone()
+		conn.close()
+		return user!=None
+
 
 	@property
 	def password(self):
@@ -35,15 +81,7 @@ class DataField(object):
 		self._password = value
 		self.passwordHash = hashlib.sha224(value).hexdigest()
 
-	@property
-	def isRoot(self):
-		return self._isRoot
-	@isRoot.setter
-	def isRoot(self, value):
-		"""Setter that only sets isRoot to 0 or 1"""
-		if value==0: self._isRoot = 0
-		else: self._isRoot = 1
-	
+
 
 class DropDownMenu(object):
 	"""docstring for DropDownMenu"""
@@ -58,3 +96,4 @@ class DropDownMenu(object):
 
 	def searchOption(self, string):
 		"""returns the list of options that could autocomplete the passed string"""
+
