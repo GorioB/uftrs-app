00208590735ad64e4dcba4ad447d62284cab3f1e
gorio <gmcbuencamino@gmail.com>
2014-06-28 21:14:22 +0800

    Modified docbuilder to work with pyinstaller.

--- interface/docbuilder.py
+++ interface/docbuilder.py
@@ -1,9 +1,18 @@
 from docx import Document
 from docx.shared import Inches
+import os
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
 
+    return os.path.join(base_path, relative_path)
 # for testing with pyinstaller
 def createDummyDocument():
-	document = Document()
+	document = Document(resource_path(os.path.join("assets","default.docx")))
 
 	document.add_heading('Document Title', 0)
 
