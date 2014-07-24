11206ab323a5b2ab283149ae04ba7d62c649c269
gorio <gmcbuencamino@gmail.com>
2014-07-25 01:08:32 +0800

    Make note numbers mandatory and fix typo in gsb.

--- lib/getStartingBalance.py
+++ lib/getStartingBalance.py
@@ -1,13 +1,17 @@
 from models import COCPNote,OME,OONote,LTINote
+from timeFuncs import *
 def getStartBalance(app,startDate):
 	lastBalanceDate, balance = app.balance
 	lastBalanceDate = int(lastBalanceDate)
-	inflows = [float(i.amount.content) for i in app.listCashReceipts() if int(i.dateOfTransaction.content)<startDate and int(i.dateOfTransaction.content)>lastBalanceDate]
+
+	for i in app.listCashReceipts():
+		print startDate, i.dateOfTransaction.content, lastBalanceDate
+	inflows = [float(i.amount.content) for i in app.listCashReceipts() if int(i.dateOfTransaction.content)<int(startDate) and int(i.dateOfTransaction.content)>lastBalanceDate]
 	outflowTypes = [OME,OONote,LTINote]
 	outflows = []
 	for j in outflowTypes:
-		outflows+=[float(i.amount.content) for i in app._listGeneral(j) if int(i.dateOfTransaction.content)<startDate and int(i.dateOfTransaction.content)>lastBalanceDate]
-	outflows+=[float(i.amount.content) for i in app._listGeneral(COCPNote) if int(i.dateOfTransaction.content)<startDate and int(i.dateOfTransaction.content)>lastBalanceDate and i.flowDirection.content=="Outflow"]
+		outflows+=[float(i.amount.content) for i in app._listGeneral(j) if int(i.dateOfTransaction.content)<int(startDate) and int(i.dateOfTransaction.content)>lastBalanceDate]
+	outflows+=[float(i.amount.content) for i in app._listGeneral(COCPNote) if int(i.dateOfTransaction.content)<int(startDate) and int(i.dateOfTransaction.content)>lastBalanceDate and i.flowDirection.content=="Outflow"]
 	netFlow = sum(inflows)-sum(outflows)
 
 	return balance+netFlow
